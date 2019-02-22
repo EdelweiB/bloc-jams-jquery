3177249e0572f3694944d6c5e7e59f47a273fbdf
Unknown <raisa.cabrera@case.edu>
2019-02-22 16:41:38 -0500

    support commit 2

--- scripts/helper.js
+++ scripts/helper.js
@@ -1,6 +1,6 @@
 class Helper {
   playPauseAndUpdate(song) {
-    const totalTime = song.duration
+    const totalTime = player.currentlyPlaying.duration
     $('#time-control .total-time').text( totalTime );
     return player.playPause(song)
   }
